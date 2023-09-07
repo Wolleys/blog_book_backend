93ce5eaca7fad8cd207bf5a5f6878d31a85636b6
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-09-08 01:11:24 +0300

    Add category routes

--- src/v1/routes/index.js
+++ src/v1/routes/index.js
@@ -5,10 +5,12 @@ const router = express.Router();
 const v1AuthRouter = require("./auth");
 const v1UserRouter = require("./users");
 const v1PostRouter = require("./posts");
+const v1CategoryRouter = require("./categories");
 
 // Use route handlers
 router.use("/api/v1/auth", v1AuthRouter);
 router.use("/api/v1/users", v1UserRouter);
 router.use("/api/v1/posts", v1PostRouter);
+router.use("/api/v1/categories", v1CategoryRouter);
 
 module.exports = router;
