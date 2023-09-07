c41ff7d3b4ac46a1d719229d668e119455675008
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-09-07 17:20:16 +0300

    Add models

---
+++ src/models/Post.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const PostSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        desc: {
+            type: String,
+            required: true,
+        },
+        photo: {
+            type: String,
+            required: false,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        categories: {
+            type: Array,
+            required: false,
+        },
+    },
+    { timestamps: true }
+);
+
+const PostModel = model("Post", PostSchema);
+
+module.exports = PostModel;
