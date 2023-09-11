db806046496e093cdf4a6ed295afef1af17dfd21
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-09-11 08:53:51 +0300

    Enable CORS

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
-const path = require("path");
+
+const corsOptions = require("./config/cors");
 const server = require("./config/server");
 const routes = require("./v1/routes");
 
 // Middlewares
 app.use(express.json());
+app.use(cors(corsOptions));
 
 // Serve static files from the 'uploads' folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
