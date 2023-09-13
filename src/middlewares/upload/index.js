b512844cf7d73705f43ebf6eac0d02a197c6bdb4
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-09-13 10:31:10 +0300

    Update profilePic route and filename upload middleware

--- src/middlewares/upload/index.js
+++ src/middlewares/upload/index.js
@@ -1,12 +1,17 @@
+const path = require("path");
 const multer = require("multer");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "src/uploads/"); // Store files in the 'uploads' folder
+        const postDest = "src/uploads/posts";
+        const profileDest = "src/uploads/profile";
+        const dest = file.fieldname === "post" ? postDest : profileDest;
+        cb(null, dest);
     },
     filename: (req, file, cb) => {
         const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-        cb(null, file.fieldname + "-" + uniqueSuffix + "-" + file.originalname);
+        const fileExtName = path.extname(file.originalname);
+        cb(null, file.fieldname + "-" + uniqueSuffix + fileExtName);
     },
 });
 
--- src/v1/routes/profilePic/index.js
+++ src/v1/routes/profilePic/index.js
@@ -2,47 +2,52 @@ const fs = require("fs");
 const path = require("path");
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 const User = require("../../../models/User");
 const upload = require("../../../middlewares/upload");
 
 //UPLOAD
-router.post("/:id", upload.single("image"), async (req, res) => {
+router.post("/:id", upload.single("profile"), async (req, res) => {
+    let image;
     try {
-        let image = req.file;
+        image = req.file;
         const userId = req.params.id;
 
         if (!image) {
             return res.status(400).json("Select a photo to upload!");
         }
 
-        if (userId) {
-            const user = await User.findById(userId);
-
-            if (!user) {
-                if (image) {
-                    fs.unlinkSync(
-                        path.join(__dirname, "../../../uploads/", image.filename)
-                    );
-                }
-                return res.status(404).json({ error: "User not found" });
-            }
-
-            user.profilePic = image.filename;
-            await user.save();
-            res.json({ message: "Image uploaded successfully!" });
-        } else {
+        if (!mongoose.Types.ObjectId.isValid(userId)) {
+            // Check if userId is not a valid ObjectId
             if (image) {
                 fs.unlinkSync(
-                    path.join(__dirname, "../../../uploads/", image.filename)
+                    path.join(__dirname, "../../../uploads/profile/", image.filename)
                 );
             }
-            res.status(401).json({ error: "Authentication required" });
+            return res.status(404).json({ error: "Invalid params" });
         }
+
+        const user = await User.findById(userId);
+
+        if (!user) {
+            if (image) {
+                fs.unlinkSync(
+                    path.join(__dirname, "../../../uploads/profile/", image.filename)
+                );
+            }
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        user.profilePic = image.filename;
+        await user.save();
+        res.json({ message: "Image uploaded successfully!" });
     } catch (error) {
         if (image) {
-            fs.unlinkSync(path.join(__dirname, "../../../uploads/", image.filename));
+            fs.unlinkSync(
+                path.join(__dirname, "../../../uploads/profile/", image.filename)
+            );
         }
-        console.error(error);
+        console.log(error);
         res.status(500).json({ error: "Image upload failed" });
     }
 });
